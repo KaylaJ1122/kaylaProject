a8cd23fc0f89438fa50510e528fa37df04a08f47
KaylaJ1122 <34862424+KaylaJ1122@users.noreply.github.com>
2021-01-18 17:42:17 +0900

    updateToday

--- src/main/java/com/kaylaproject/example/domain/User.java
+++ src/main/java/com/kaylaproject/example/domain/User.java
@@ -1,5 +1,9 @@
 package com.kaylaproject.example.domain;
 
+import java.util.Collections;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 public class User {
 
 }
